052b20dcda77305bb3b4ec334a39b3e5389ffa77
Montasir Maruf <maruf@cheyenne6.cheyenne.ucar.edu>
2021-09-01 13:55:08 -0600

    Compute wind chill factor

--- mysci.py
+++ mysci.py
@@ -1,5 +1,13 @@
 # Initialize data variable
-data = []
+columns = {'data':0, 'time':1, 'tempout':2, 'windspeed':7}
+ 
+types ={'tempout':float, 'windspeed':float}
+
+data = {}
+for column in columns:
+    data[column] = []
+
+filename = 'data/wxobs20170821.txt'
 
 filename = "data/wxobs20170821.txt"
 
@@ -7,14 +15,32 @@ filename = "data/wxobs20170821.txt"
 with open(filename, 'r') as datafile:
      # read the first three lines 
     for _ in range(3):
-        print(_)
-        datafile.readline()         
-
+        headerline =  datafile.readline()         
+        print(headerline) 
     # Read amd parse the rest of the file
     for line in datafile:
-        datum = line.split()
-        data.append(datum)
+        split_line = line.split()
+        for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+        
 
-# Debug
-print(data[5:8][0]) 
-print(data[5])  
+def compute_windchill(t, v):
+    a = 35.74
+    b = 0.6215
+    c= 35.75
+    d = 0.4275
+
+    v16 = v ** 0.16
+   
+    wci = a = (b*t) - (c * v16) + (d * t * v16)
+    return wci
+
+#DEBUG
+windchill = []
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+    windchill.append(compute_windchill(temp, windspeed))
+
+print(windchill)
